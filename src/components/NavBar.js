0488cb31899c30168837ced10036864943509234
Juan <juanmarten99@gmail.com>
2023-05-12 14:23:16 -0300

    fix navbar links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,19 @@
+import { Link } from 'react-router-dom';
+
 export default function NavBar(){
     return(
         <nav class="navbar navbar-expand-lg border-bottom border-3 mb-3">
             <div class="container-fluid">
-                <a class="navbar-brand" href="/">Master Gaming</a>
+                <Link class="navbar-brand" to="/">Master Gaming</Link>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div class="navbar-nav">
-                    <a class="nav-link" href="/">Inicio</a>
-                    <a class="nav-link" href="/products">Productos</a>
-                    <a class="nav-link" href="/cart">Carrito</a>
-                    <a class="nav-link" href="/orders">Pedidos</a>
+                    <Link class="nav-link" to="/">Inicio</Link>
+                    <Link class="nav-link" to="/products">Productos</Link>
+                    <Link class="nav-link" to="/cart">Carrito</Link>
+                    <Link class="nav-link" to="/orders">Pedidos</Link>
                 </div>
                 </div>
             </div>
