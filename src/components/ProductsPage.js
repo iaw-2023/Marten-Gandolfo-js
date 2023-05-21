86323ce7c8ab4534eed1fc62b7cfc82ad8115c09
Matias Emiliano Gandolfo <matias_gandolfo17@hotmail.com>
2023-05-21 14:55:25 -0300

    search functionality added to products page

--- src/components/ProductsPage.js
+++ src/components/ProductsPage.js
@@ -3,13 +3,28 @@ import { Link } from 'react-router-dom';
 
 export default function ProductsPage(){
     const [products, setProducts] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+    const [searchResults, setSearchResults] = useState([]);
 
     useEffect(() => {
         fetch('https://marten-gandolfo-laravel.vercel.app/_api/products')
           .then(response => response.json())
-          .then(data => setProducts(data))
+          .then((data) => {
+            setProducts(data);
+            setSearchResults(data);
+          })
           .catch(error => console.log(error));
       }, []);
+
+    const handleSearch = (event) => {
+        const term = event.target.value.toLowerCase();
+        setSearchTerm(term);
+
+        const results = products.filter((product) =>
+            product.name.toLowerCase().includes(term)
+        );
+        setSearchResults(results);
+    };
 
     if (products.length === 0) {
         return <div>Cargando...</div>;
@@ -18,19 +33,30 @@ export default function ProductsPage(){
     return (
         <div>
             <h1>Productos</h1>
-            <table id="products" class="table table-striped table-bordered shadow-lg"> 
+
+            <input
+                type="text"
+                placeholder="Buscar producto..."
+                value={searchTerm}
+                onChange={handleSearch}
+                style={{ marginBottom: '1rem' }}
+            />
+
+            <table className="table table-striped table-bordered shadow-lg">
                 <tbody>
-                    {products.map(product => (
-                        <tr>
-                            <td>
-                                <Link to={'/products/' + product.id}><img src={product.product_image} alt='' width="150"/></Link>
-                            </td>
-                            <td>
-                                <Link to={'/products/' + product.id}>{product.name} </Link>
-                            </td>
-                            <td>${product.price}</td>
-                        </tr>
-                    ))}
+                {searchResults.map((product) => (
+                    <tr key={product.id}>
+                    <td>
+                        <Link to={'/products/' + product.id}>
+                        <img src={product.product_image} alt="" width="150" />
+                        </Link>
+                    </td>
+                    <td>
+                        <Link to={'/products/' + product.id}>{product.name}</Link>
+                    </td>
+                    <td>${product.price}</td>
+                    </tr>
+                ))}
                 </tbody>
             </table>
         </div>
