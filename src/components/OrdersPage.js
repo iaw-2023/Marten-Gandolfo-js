47f4fcaeb5e2451ddddddea7c648c1d5f3ccc67f
Matias Emiliano Gandolfo <matias_gandolfo17@hotmail.com>
2023-05-21 14:29:47 -0300

    added option to search order by token

--- src/components/OrdersPage.js
+++ src/components/OrdersPage.js
@@ -1,7 +1,37 @@
-export default function OrdersPage(){
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+function OrdersPage() {
+    const [token, setToken] = useState('');
+    const [isTokenValid, setIsTokenValid] = useState(false);
+
+    const handleTokenChange = event => {
+        setToken(event.target.value);
+    };
+
+    const handleButtonClick = () => {
+        window.location.href = `/orders/${token}/details`;
+    };
+
+    useEffect(() => {
+        setIsTokenValid(token !== '');
+    }, [token]);
+
     return (
         <div>
             <h1>Pedidos</h1>
+            <p>Ingrese el token de su pedido a continuacion para ver mas informacion.</p>
+            <input
+                type="text"
+                value={token}
+                onChange={handleTokenChange}
+                placeholder="Ingrese su token"
+            />
+            
+            <button className="button-link" onClick={handleButtonClick} disabled={!isTokenValid}>Buscar</button>
+
         </div>
     );
 }
+  
+export default OrdersPage;
