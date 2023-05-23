8be43fd47c4798c8bbe213b024ab535725513e25
Juan <juanmarten99@gmail.com>
2023-05-23 13:38:49 -0300

    fix order page

--- src/components/OrderDetailsPage.js
+++ src/components/OrderDetailsPage.js
@@ -15,6 +15,7 @@ function OrderDetailsPage() {
       })
       .then(data => {
         setIsLoading(false);
+        console.log(data);
         return setOrder(data);
       })
       .catch(error => {
@@ -28,7 +29,7 @@ function OrderDetailsPage() {
   }
 
   if (!order) {
-    return <div>Orden no encontrada</div>;
+    return <div>Pedido no encontrado</div>;
   }
 
   order.order_details.forEach(detail => {
@@ -37,15 +38,15 @@ function OrderDetailsPage() {
 
   return (
     <div>
-      <h1>Detalles de su orden</h1>
-      <p>Numero de orden: {order.id}</p>
-      <p>Fecha de compra: {order.order_date}</p>
+      <h1>Detalles de su pedido</h1>
+      <p>Numero de pedido: {order.id}</p>
+      <p>Fecha: {order.order_date}</p>
 
       <table>
         <thead>
             <tr>
-                <th>IMG</th>
-                <th>Nombre Producto</th>
+                <th></th>
+                <th>Producto</th>
                 <th>Unidades</th>
                 <th>Subtotal</th>
             </tr>
@@ -55,21 +56,21 @@ function OrderDetailsPage() {
           {order.order_details.map(detail => (
             <tr key={detail.product.id}>
               <td>
-                <Link to={'/products/' + detail.product.id}><img src={""} alt='' width="150" /></Link>
+                <Link to={'/products/' + detail.product.id}><img src={detail.product.product_image} alt='' width="150" /></Link>
               </td>
               <td>
                 <Link to={'/products/' + detail.product.id}>{detail.product.name}</Link>
               </td>
 
               <td>{detail.units}</td>
-              <td>{detail.subtotal}</td>
+              <td>${detail.subtotal}</td>
             </tr>
           ))}
           <tr>
             <td></td>
             <td></td>
             <td>TOTAL:</td>
-            <td>{totalCost}</td>
+            <td>${totalCost}</td>
           </tr>
         </tbody>
     </table>
