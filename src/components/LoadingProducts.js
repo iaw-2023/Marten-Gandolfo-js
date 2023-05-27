b1b9fbeb24029a7bdfb81ce39d27043ab14dadc6
Matias Emiliano Gandolfo <matias_gandolfo17@hotmail.com>
2023-05-27 01:07:21 -0300

    added style to products page + nav-bar

---
+++ src/components/LoadingProducts.js
@@ -0,0 +1,75 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import '../App.css'
+
+export default function LoadingSpinner(){
+    return (
+        <>
+
+            <div class="card-container">
+                <div class="card" aria-hidden="true" style={{width: "18rem"}}>
+                        <img class="card-img-top placeholder" style={{ height: "12rem", objectFit: "cover" }}></img>
+                        <div class="card-body" style={{ height: "8rem" }}>
+                            <h5 class="card-title placeholder-glow">
+                                <span class="placeholder col-6"></span>
+                            </h5>
+                            <p class="card-text placeholder-glow">
+                                <span class="placeholder col-7"></span>
+                                <span class="placeholder col-4"></span>
+                                <span class="placeholder col-4"></span>
+                                <span class="placeholder col-6"></span>
+                                <span class="placeholder col-8"></span>
+                            </p>
+                    </div>
+                </div>
+
+                <div class="card" aria-hidden="true" style={{width: "18rem"}}>
+                        <img class="card-img-top placeholder" style={{ height: "12rem", objectFit: "cover" }}></img>
+                        <div class="card-body" style={{ height: "8rem" }}>
+                            <h5 class="card-title placeholder-glow">
+                                <span class="placeholder col-6"></span>
+                            </h5>
+                            <p class="card-text placeholder-glow">
+                                <span class="placeholder col-7"></span>
+                                <span class="placeholder col-4"></span>
+                                <span class="placeholder col-4"></span>
+                                <span class="placeholder col-6"></span>
+                                <span class="placeholder col-8"></span>
+                            </p>
+                    </div>
+                </div>
+
+                <div class="card" aria-hidden="true" style={{width: "18rem"}}>
+                        <img class="card-img-top placeholder" style={{ height: "12rem", objectFit: "cover" }}></img>
+                        <div class="card-body" style={{ height: "8rem" }}>
+                            <h5 class="card-title placeholder-glow">
+                                <span class="placeholder col-6"></span>
+                            </h5>
+                            <p class="card-text placeholder-glow">
+                                <span class="placeholder col-7"></span>
+                                <span class="placeholder col-4"></span>
+                                <span class="placeholder col-4"></span>
+                                <span class="placeholder col-6"></span>
+                                <span class="placeholder col-8"></span>
+                            </p>
+                    </div>
+                </div>
+
+                <div class="card" aria-hidden="true" style={{width: "18rem"}}>
+                        <img class="card-img-top placeholder" style={{ height: "12rem", objectFit: "cover" }}></img>
+                        <div class="card-body" style={{ height: "8rem" }}>
+                            <h5 class="card-title placeholder-glow">
+                                <span class="placeholder col-6"></span>
+                            </h5>
+                            <p class="card-text placeholder-glow">
+                                <span class="placeholder col-7"></span>
+                                <span class="placeholder col-4"></span>
+                                <span class="placeholder col-4"></span>
+                                <span class="placeholder col-6"></span>
+                                <span class="placeholder col-8"></span>
+                            </p>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
