13f678b586c575986c652e560b1ee5806b3fada5
Juan <juanmarten99@gmail.com>
2023-05-30 13:16:31 -0300

    update error message component

--- src/components/ErrorMessage.js
+++ src/components/ErrorMessage.js
@@ -1,5 +1,5 @@
 export default function ErrorMessage({message}){
     return (
-        <div class="alert alert-danger" role="alert">{message}</div>
+        <div class="alert alert-danger m-5" role="alert">{message}</div>
     );
 }
